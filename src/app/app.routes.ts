e2d0a6342e233ecc709b6f7d3b7d741a95b5f3ab
Marco636400 <marcocovolo17@gmail.com>
2026-06-10 17:00:05 +0200

    covolo commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
-import { RubricaComponent } from './components/rubrica/rubrica';
-import { ContattoComponent } from './components/contatto/contatto';
+
+import { RubricaPagina } from './rubrica-pagina/rubrica-pagina';
+import { PaginaAggiungiComponent } from './pagina-aggiungi/pagina-aggiungi';
+import { PaginaModificaComponent } from './pagina-modica-contatto/pagina-modica-contatto';
+import { PaginaSalvaComponent } from './pagina-salva/pagina-salva';
+
+
+
 
 export const routes: Routes = [
-  { path: '', component: RubricaComponent },
-  { path: 'contatto', component: ContattoComponent }
+  { path: '', component: RubricaPagina },  // pagina iniziale
+  { path: 'rubrica-pagina', component: RubricaPagina },
+
+  // pagina aggiungi
+  { path: 'pagina-aggiungi', component: PaginaAggiungiComponent },
+
+  // pagina modifica
+  { path: 'pagina-modica-contatto', component: PaginaModificaComponent },
+
+  // pagina salva
+  { path: 'pagina-salva', component: PaginaSalvaComponent }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Titolo } from "./titolo/titolo";
+import { BTNaggiungiFam } from "./btnaggiungi-fam/btnaggiungi-fam";
+import { RubricaPagina } from "./rubrica-pagina/rubrica-pagina";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Titolo, BTNaggiungiFam, RubricaPagina],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  protected readonly title = signal('parteFrontend');
 }
