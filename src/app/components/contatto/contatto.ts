5489c0059560c3e628ba4fef84618a78c92d90a4
smartini-a11y <smartini@chilesotti.it>
2026-06-10 17:28:23 +0200

    selezoione dei familiari per ogni contatto e visualizzazione

--- src/app/components/contatto/contatto.ts
+++ src/app/components/contatto/contatto.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'; // <-- Import this
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-contatto',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule],
   templateUrl: './contatto.html',
   styleUrl: './contatto.css'
 })
@@ -13,6 +13,8 @@ export class ContattoComponent implements OnInit {
   // Creating an object to hold the data so contatto.html can see it
   c: any = {};
   listaContatti: any[] = [];
+
+  messaggio = '';
 
   constructor(private route: ActivatedRoute, private router: Router) {
     // Recuperiamo la lista che arriva dall'altra pagina
@@ -27,6 +29,7 @@ export class ContattoComponent implements OnInit {
     // Look at the URL and grab the parameters
     this.route.queryParams.subscribe(params => {
       this.c = {
+        id: params['id'],
         nome: params['nome'],
         cognome: params['cognome'],
         telefono: params['telefono'],
@@ -34,4 +37,52 @@ export class ContattoComponent implements OnInit {
       };
     });
   }
+  
+  checkContatto(item: any): boolean {
+    if (item.nome === this.c.nome && item.cognome === this.c.cognome) {
+      return false;
+    }
+    return true;
+  }
+
+    familiariSelezionati: any[] = [];
+
+  condividiContatto()
+    {
+      const selectedElement=document.getElementById('condivisione') as HTMLSelectElement;
+      const selectedId = Number(selectedElement.value);
+      
+      const familiare = this.listaContatti.find(c => Number(c.id)=== selectedId);
+
+      if (!familiare) 
+      {
+        return;
+      }
+      // Evito duplicati
+      if (!this.familiariSelezionati.includes(familiare)) 
+        {
+          this.familiariSelezionati.push(familiare);
+        }
+
+      //ciclo per costruire il messaggio
+      let lista = '';
+
+      for (let f of this.familiariSelezionati) 
+        {
+          lista += `${f.nome} ${f.cognome}\n`;    
+        }
+
+        this.messaggio = lista ;
+        
+      }
+
+       rimuoviFamiliare(familiare: any) 
+      {
+          
+      }
+
+      vediDettaglio(familiare: any)
+      {
+
+      }
 }
--- src/app/components/rubrica/rubrica.ts
+++ src/app/components/rubrica/rubrica.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import{RouterLink} from '@angular/router';
 
 interface Contatto {
+  id: number;
   nome: string;
   cognome: string;
   telefono: string;
@@ -20,20 +21,24 @@ interface Contatto {
 //gestisco la lista che creo in una classe e faccio i metodi per aggiungere, modificare e cancellare i contatti
  export class RubricaComponent {
   listaContatti: Contatto[] = [
-    { nome: 'Mario', cognome: 'Rossi', telefono: '333123456', email: 'mario.rossi@email.com' },
-    { nome: 'Luigi', cognome: 'Verdi', telefono: '333987654', email: 'luigi.verdi@email.com' },
-    { nome: 'Giulia', cognome: 'Bonani', telefono: '333134567', email: 'giulia.bonani@email.com' },
-    { nome: 'Francesca', cognome: 'Bianchi', telefono: '333765432', email: 'francesca.bianchi@email.com' }
+    { id: 1, nome: 'Mario', cognome: 'Rossi', telefono: '333123456', email: 'mario.rossi@email.com' },
+    { id: 2, nome: 'Luigi', cognome: 'Verdi', telefono: '333987654', email: 'luigi.verdi@email.com' },
+    { id: 3, nome: 'Giulia', cognome: 'Bonani', telefono: '333134567', email: 'giulia.bonani@email.com' },
+    { id: 4, nome: 'Francesca', cognome: 'Bianchi', telefono: '333765432', email: 'francesca.bianchi@email.com' }
   ];
 
   nuovoNome: string = '';
   nuovoCognome: string = '';
   nuovoTelefono: string = '';
   nuovaEmail: string = '';
+  ultimoId=5;
 
-  aggiungiContatto() {
+  aggiungiContatto() 
+  {
     if (this.nuovoNome && this.nuovoCognome && this.nuovoTelefono && this.nuovaEmail) {
-      this.listaContatti.push({
+      this.listaContatti.push
+      ({
+        id: this.ultimoId++,
         nome: this.nuovoNome,
         cognome: this.nuovoCognome,
         telefono: this.nuovoTelefono,
